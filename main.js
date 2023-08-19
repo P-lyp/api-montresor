343cbf4d4a48a72417862db7bab7f3d694ad4762
p-lyp <felipealves13tga@hotmail.com>
2023-08-19 18:26:40 -0400

    correcao ordenacao

--- main.js
+++ main.js
@@ -36,7 +36,8 @@ app.put("/gastos", async (req, res) => {
 app.get("/gastos", async (req, res) => {
     const snapshot = await gastos.get();
     listaGastos = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.send(listaGastos).orderBy("data", "desc");
+    listaGastos.orderBy("data", "desc");
+    res.send(listaGastos);
 });
 
 app.listen(process.env.PORT || 3000);
