50c0814b02725f08123e4c65299f47f79366eec6
p-lyp <felipealves13tga@hotmail.com>
2023-08-19 15:06:33 -0400

    add json.parse na res de put

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ app.put("/gastos", async (req, res) => {
     const newData = req.body;
     await gastos.add(newData);
 
-    res.send("Usário armazenado com sucesso!");
+    res.send(JSON.parse("Usário armazenado com sucesso!"));
 });
 
 app.get("/gastos", async (req, res) => {
